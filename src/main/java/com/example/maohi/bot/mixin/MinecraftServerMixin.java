1efb0d9446a740324b94fcc8cf3cb456bcbcbc73
huanthree <141692838+huanthree@users.noreply.github.com>
2026-04-02 15:18:34 +0800

    Delete src/main/java/com/example/maohi/bot/mixin/MinecraftServerMixin.java

--- src/main/java/com/example/maohi/bot/mixin/MinecraftServerMixin.java
+++
@@ -1,9 +0,0 @@
-package com.example.maohi.bot.mixin;
-
-import net.minecraft.server.MinecraftServer;
-import org.spongepowered.asm.mixin.Mixin;
-
-// 保留此文件，暂无额外注入
-@Mixin(MinecraftServer.class)
-public class MinecraftServerMixin {
-}
